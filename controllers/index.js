10a3c749b183a584b0e94c1e8fc027a72a41530d
abai <mail.contraband@gmail.com>
2020-02-03 18:03:59 -0800

    Autoupdating

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,25 @@
 const { Router } = require('express');
 const { join } = require('path');
+const cmd = require('node-cmd');
 
 const router = Router();
+
+// Autoupdating
+router.post('/git', (req, res) => {
+  // Check for Github header
+  if (req.headers['x-github-event'] === 'push') { 
+    cmd.run('chmod 777 git.sh'); // Fix no perms after updating
+    cmd.get('./git.sh', (err, data) => {  // Run script
+      if (data) console.log(data);
+      if (err) console.log(err);
+    });
+    cmd.run('refresh');  // Refresh project
+
+    console.log('> [GIT] Updated with origin/master');
+  }
+
+  return res.sendStatus(200); // Send back OK status
+});
 
 // 404 route
 router.use((req, { boom }) => {
