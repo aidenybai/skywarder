624567dccf922984a9f6e6be1dfd36b7bb9f892f
abai <mail.contraband@gmail.com>
2020-03-07 18:59:48 -0800

    Fix daemons and add pnpm support

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const { join } = require('path');
@@ -15,31 +15,31 @@ const app = express();
 // Import middleware
 
 // Good to enable behind a reverse proxy
-app.set('trust proxy', 1); 
+app.set('trust proxy', 1);
 
 // Express error messages
 app.use(require('express-boom')());
 
 // Enables Cross-Origin Resource Sharing
-app.use(require('cors')()); 
+app.use(require('cors')());
 
 // G-Zip codec static resources sent to user
-app.use(require('compression')()); 
+app.use(require('compression')());
 
 // Prevents malicious headers, XSS, and clickjacking
-app.use(require('helmet')()); 
+app.use(require('helmet')());
 
 // POST request support (encoding)
-app.use(require('body-parser').urlencoded({ extended: true })); 
+app.use(require('body-parser').urlencoded({ extended: true }));
 
 // POST request support (read as application/json)
-app.use(require('body-parser').json()); 
+app.use(require('body-parser').json());
 
 // Comprehensive HTTP logger
 app.use(require('morgan')('combined'));
 
 // Support for different ways of passing tokens
-app.use(require('express-bearer-token')()); 
+app.use(require('express-bearer-token')());
 
 // Setup ratelimiting on v1 with 50req/10s
 app.use(
@@ -68,13 +68,10 @@ app.use(
 );
 
 // Connect to MongoDB
-const db = connect(
-  process.env.DB_URL,
-  {
-    useNewUrlParser: true, // Updated URL parser for mongoose, might as well use it
-    useUnifiedTopology: true, // Use server discover and monitoring engine for MongoClient
-  }
-);
+const db = connect(process.env.DB_URL, {
+  useNewUrlParser: true, // Updated URL parser for mongoose, might as well use it
+  useUnifiedTopology: true, // Use server discover and monitoring engine for MongoClient
+});
 
 // Register routes
 app.use('/v1', v1Routes);
